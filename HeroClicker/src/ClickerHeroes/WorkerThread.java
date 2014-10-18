8c17614a05f747015894c694a6624e2b4bc8945a
Anders Söderholm <andso518@student.liu.se>
2014-10-18 16:57:31 +0200

    most values updated to laptop values, added phases

--- HeroClicker/src/ClickerHeroes/WorkerThread.java
+++ HeroClicker/src/ClickerHeroes/WorkerThread.java
@@ -24,6 +24,11 @@ public class WorkerThread extends Thread implements ActionListener{
 	private Timer phase1Timer;
 	private Timer phase2Timer;
 	private Timer phase4Timer;
+	private Timer phase5Timer;
+	private Timer phase6Timer;
+	private Timer phase7Timer;
+	private Timer phase8Timer;
+	
 
 	Robot robert;
 	
@@ -58,7 +63,7 @@ public class WorkerThread extends Thread implements ActionListener{
 						if(phase1Timer.isRunning()){
 							phase1Timer.stop();
 							if(phase2Timer == null){
-								phase2Timer = new Timer(TimeToMilli(4,0), this);
+								phase2Timer = new Timer(TimeToMilli(2,0), this);
 								phase2Timer.setActionCommand("nextPhase");
 								phase2Timer.start();
 							}else{
@@ -74,11 +79,59 @@ public class WorkerThread extends Thread implements ActionListener{
 							phase2Timer.stop();
 						}
 						else if(phase == 5){
-							System.out.println("Phase(5): World Ascention entered,		World("+ world_nr +")");
+							System.out.println("Phase(5): Frostleaf entered,		World("+ world_nr +")");
+							phase4Timer.stop();
+							if(phase5Timer == null){
+								phase5Timer = new Timer(TimeToMilli(1,10), this);
+								phase5Timer.setActionCommand("nextPhase");
+								phase5Timer.start();
+							}else{
+								phase5Timer.restart();
+							}
+						}
+						else if(phase == 6){
+							System.out.println("Phase(6): Samurai and seer entered,		World("+ world_nr +")");
+							phase5Timer.stop();
+							if(phase6Timer == null){
+								phase6Timer = new Timer(TimeToMilli(3,0), this);
+								phase6Timer.setActionCommand("nextPhase");
+								phase6Timer.start();
+							}else{
+								phase6Timer.restart();
+							}
+						}
+						else if(phase == 7){
+							System.out.println("Phase(7): Natalia and Mercedes entered,		World("+ world_nr +")");
+							phase6Timer.stop();
+							if(phase7Timer == null){
+								phase7Timer = new Timer(TimeToMilli(2,0), this);
+								phase7Timer.setActionCommand("nextPhase");
+								phase7Timer.start();
+							}else{
+								phase7Timer.restart();
+							}
+						}
+						else if(phase == 8){
+							System.out.println("Phase(8): TreebeastIvanBrittany again entered,		World("+ world_nr +")");
+							phase7Timer.stop();
+							if(phase8Timer == null){
+								phase8Timer = new Timer(TimeToMilli(2,0), this);
+								phase8Timer.setActionCommand("nextPhase");
+								phase8Timer.start();
+							}else{
+								phase8Timer.restart();
+							}
+						}
+						else if(phase == 9){
+							System.out.println("Phase(7): World Ascention entered,		World("+ world_nr +")");
 							//Time for a new word, stop all phaseTimers
 							phase1Timer.stop();
 							phase2Timer.stop();
 							phase4Timer.stop();			
+							phase5Timer.stop();
+							phase6Timer.stop();
+							phase7Timer.stop();
+							phase8Timer.stop();
 						}
 					}
 					else if(eventArg.getActionCommand().contentEquals("checkProgress")){
@@ -92,20 +145,20 @@ public class WorkerThread extends Thread implements ActionListener{
 						CheckProgressOn();
 						//Initial clicking to get enough gold to buy a hero
 						robert.mouseMove(1382, 600);
-						AutoClick(55,4);
+						AutoClick(25,4);
 
 						BuyTreeleaf();
 						
 						if(phase1Timer == null){
-							phase1Timer = new Timer(TimeToMilli(11,0), this);
+							phase1Timer = new Timer(TimeToMilli(3,30), this);
 							phase1Timer.setActionCommand("nextPhase");
 							phase1Timer.start();
 						}else{
 								phase1Timer.restart();
 						}
 						
-						phase1Timer.setActionCommand("nextPhase");
-						phase1Timer.start();
+						//phase1Timer.setActionCommand("nextPhase");
+						//phase1Timer.start();
 		
 						System.out.println("Phase: First run(1) entered,		World("+ world_nr +")");
 						phase = 1;
@@ -120,11 +173,11 @@ public class WorkerThread extends Thread implements ActionListener{
 						buyUpgrades();
 					}
 					else if(phase == 3){
-						MassLevelUp();
+						QuickLevelUp();
 						phase =  4;
 						System.out.println("Phase(4): TreeBeast, Ivan and Brittany entered,		World("+ world_nr +")");
 						if(phase4Timer == null){
-							phase4Timer = new Timer(TimeToMilli(25,0), this);
+							phase4Timer = new Timer(TimeToMilli(10,0), this);
 							phase4Timer.setActionCommand("nextPhase");
 							phase4Timer.start();
 						}else{
@@ -137,7 +190,19 @@ public class WorkerThread extends Thread implements ActionListener{
 						BuyTreeBeastIVanBrittany();
 					}
 					else if(phase == 5){
-						MassLevelUp();
+						BuyFrostLeaf();
+					}
+					else if(phase == 6){
+						BuySamuraiSeer();
+					}
+					else if(phase == 7){
+						BuyNataliaMercedes();
+					}
+					else if(phase == 8){
+						BuyTreeBeastIVanBrittany();
+					}
+					else if(phase == 9){
+						QuickLevelUp();
 						AscendTheWorld();
 						phase = 0;
 						
@@ -174,12 +239,12 @@ public class WorkerThread extends Thread implements ActionListener{
 		}
 		
 		public void CheckProgressOn(){
-			robert.mouseMove(1784, 419);
-			if(ComparePixelColor(new Color(196,160,72),1285, 562)){
+			robert.mouseMove(1784, 422);
+			if(ComparePixelColor(new Color(255,222,101),1784, 422)){
 				System.out.println("Progess mode was on, no action taken");
 			}
 			else{
-				System.out.println((robert.getPixelColor(1285, 562).toString()) +" != " + (new Color(196,160,72).toString()) );
+				System.out.println((robert.getPixelColor(1784, 422).toString()) +" != " + (new Color(255,222,101).toString()) );
 				Click();
 				System.out.println("Progress mode was off, progress mode activated");
 				System.out.println("continue to check progress at " +  milliToString(ProgressCheckTimer.getInitialDelay()) + " intervalls");
@@ -215,11 +280,13 @@ public class WorkerThread extends Thread implements ActionListener{
 			robert.mouseRelease(InputEvent.BUTTON1_MASK);
 		}
 		
-		public void CtrlClick(){
+		public void CtrlClick() throws InterruptedException{
 		
 			robert.keyPress(KeyEvent.VK_CONTROL);
+			this.sleep(200);
 			robert.mousePress(InputEvent.BUTTON1_MASK);
 			robert.mouseRelease(InputEvent.BUTTON1_MASK);
+			this.sleep(200);
 			robert.keyRelease(KeyEvent.VK_CONTROL);
 		}
 		
@@ -275,9 +342,39 @@ public class WorkerThread extends Thread implements ActionListener{
 			this.sleep(750,0);
 			robert.mouseMove(245, 902);
 			CtrlClick();
-			this.sleep(750,0);
+			this.sleep(750,0);	
+		}
+		
+		public void BuySamuraiSeer() throws InterruptedException{
+			this.sleep(500,0);
+			robert.mouseMove(932, 403);
+			Click();
+			this.sleep(500,0);
+			ScrollDown(10);
 			
+			robert.mouseMove(245, 546);
+			CtrlClick();
+			this.sleep(750);
+
+			robert.mouseMove(245, 873);
+			CtrlClick();
+			this.sleep(750);
+		}
+		
+		public void BuyNataliaMercedes() throws InterruptedException{
+			this.sleep(500,0);
+			robert.mouseMove(932, 403);
+			Click();
+			this.sleep(500,0);
+			ScrollDown(15);
 			
+			robert.mouseMove(245, 793);
+			CtrlClick();
+			this.sleep(750);
+
+			robert.mouseMove(245, 953);
+			CtrlClick();
+			this.sleep(750);
 		}
 		
 		//desktop values
@@ -693,10 +790,13 @@ public class WorkerThread extends Thread implements ActionListener{
 		public void BuyFrostLeaf() throws InterruptedException{
 
 			ScrollDown(45);
-			this.sleep(750,0);
-			robert.mouseMove(240, 796);
-			CtrlClick();
-			this.sleep(750,0);
+			for(int i = 0; i < 7; ++i)
+			{
+				this.sleep(750,0);
+				robert.mouseMove(240, 796);
+				CtrlClick();
+				this.sleep(750,0);
+			}
 		}
 		
 		
@@ -722,7 +822,269 @@ public class WorkerThread extends Thread implements ActionListener{
 			++world_nr;
 		}
 
+		public void QuickLevelUp() throws InterruptedException{
+			
+			//same functionality as MassLevelUp but only buys the minimum amount required to get all upgrades 
+			ScrollUp(46);
+			this.sleep(500,0);
+			
+			//Level up Cid
+			this.sleep(750,0);
+			robert.mouseMove(240, 432);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			//Level up Treebeast
+			this.sleep(750,0);
+			robert.mouseMove(240, 593);
+			for(int i = 0; i < 7; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			//Level up Ivan
+			this.sleep(750,0);
+			robert.mouseMove(240, 756);
+			for(int i = 0; i < 7; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			//Level up brittany
+			this.sleep(750,0);
+			robert.mouseMove(240, 906);
+			for(int i = 0; i < 7; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+		
+			ScrollDown(2);
+			//Level up fisherman
+			this.sleep(750,0);
+			robert.mouseMove(240, 891);
+			for(int i = 0; i < 1; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+		
+			
+			ScrollDown(2);
+			//Level up betty
+			this.sleep(750,0);
+			robert.mouseMove(240, 901);
+			for(int i = 0; i < 1; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up samurai
+			this.sleep(750,0);
+			robert.mouseMove(240, 880);
+			for(int i = 0; i < 8; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up leon
+			this.sleep(750,0);
+			robert.mouseMove(240, 898);
+			for(int i = 0; i < 1; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up seer
+			this.sleep(750,0);
+			robert.mouseMove(240, 867);
+			for(int i = 0; i < 8; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(1);
+			//Level up alexa
+			this.sleep(750,0);
+			robert.mouseMove(240, 961);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up natalia
+			this.sleep(750,0);
+			robert.mouseMove(240, 972);
+			for(int i = 0; i < 8; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up mercedes
+			this.sleep(750,0);
+			robert.mouseMove(240, 960);
+			for(int i = 0; i < 8; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
 
+			ScrollDown(2);
+			//Level up bobby
+			this.sleep(750,0);
+			robert.mouseMove(240, 977);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up broyle
+			this.sleep(750,0);
+			robert.mouseMove(240, 949);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up george
+			this.sleep(750,0);
+			robert.mouseMove(240, 959);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up midas
+			this.sleep(750,0);
+			robert.mouseMove(240, 940);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up referi
+			this.sleep(750,0);
+			robert.mouseMove(240, 963);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up abaddon
+			this.sleep(750,0);
+			robert.mouseMove(240, 935);
+			for(int i = 0; i < 1; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up ma zhu
+			this.sleep(750,0);
+			robert.mouseMove(240, 950);
+			for(int i = 0; i < 1; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up amenhotep
+			this.sleep(750,0);
+			robert.mouseMove(240, 920);
+			for(int i = 0; i < 3; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up beastlord
+			this.sleep(750,0);
+			robert.mouseMove(240,908);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up athena
+			this.sleep(750,0);
+			robert.mouseMove(240, 907);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			ScrollDown(2);
+			//Level up aphrodite
+			this.sleep(750,0);
+			robert.mouseMove(240, 894);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up shinatobe
+			this.sleep(750,0);
+			robert.mouseMove(240, 905);
+			for(int i = 0; i < 2; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+
+			ScrollDown(2);
+			//Level up grant
+			this.sleep(750,0);
+			robert.mouseMove(240, 893);
+			for(int i = 0; i < 1; ++i){
+				CtrlClick();
+				this.sleep(300,0);
+			}
+			this.sleep(750,0);
+			
+			//buy all upgrades
+			ScrollDown(10);
+			this.sleep(750,0);
+			System.out.println("Buyin all uppgrades\n");
+			robert.mouseMove(539, 918);
+			Click();
+			this.sleep(750,0);
+			
+		}
 
 	}
 
